c5483eddeb825e0a92124aeb6d2b65e96c648b44
Marcelle Hövelmanns <marcelle.hoevelmanns@gmail.com>
2017-10-25 21:15:14 +0200

    code improvements

--- src/Client.php
+++ src/Client.php
@@ -36,12 +36,10 @@ class Client
 
   public static function getInstance()
   {
-    {
-      if (self::$instance === null) {
-        self::$instance = new self();
-      }
-      return self::$instance;
+    if (self::$instance === null) {
+      self::$instance = new self();
     }
+    return self::$instance;
   }
 
   public function init()
