6762f8e4b43143a36d4ae1892d712d378e8eb2c6
Marcelle Hövelmanns <marcelle.hoevelmanns@gmail.com>
2017-12-11 07:22:48 +0100

    code refactoring

---
+++ src/interfaces/ClientInterface.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright (C) Marcelle Hövelmanns, art solution - All Rights Reserved
+ *
+ * @file        ClientInterface.php
+ * @author      Marcelle Hövelmanns
+ * @site        http://www.artsolution.de
+ * @date        24.10.17
+ */
+
+namespace AffiliconApiClient\Interfaces;
+
+/**
+ * Interface ClientInterface
+ * @package Affilicon
+ */
+
+interface ClientInterface
+{
+
+  /**
+   * Initializes the Client
+   * @return $this
+   */
+  public function init();
+
+  /**
+   * Sets the Client ID, previously called Vendor ID
+   * @param string $id
+   * @return $this
+   */
+  public function setClientId($id);
+
+  /**
+   * Gets the Client ID, previously called Vendor ID
+   * @return string
+   */
+  public function getClientId();
+
+  /**
+   * Gets the specified country code
+   * @return string
+   */
+  public function getCountryId();
+
+  /**
+   * Sets the country code, eg. "en-US"
+   * @param $countryId
+   * @return $this
+   */
+  public function setCountryId($countryId);
+
+  /**
+   * Gets the specified user language
+   * @return $this
+   */
+  public function getUserLanguage();
+
+  /**
+   * Sets the user language, eg. "en"
+   * @param string $userLanguage
+   * @return $this
+   */
+  public function setUserLanguage($userLanguage);
+
+}
